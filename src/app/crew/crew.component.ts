cfcb6fd904e6f7a15d5182b3947c7fc670e4fdfb
Jamie Shadduck <jamie.shadduck.js@gmail.com>
2022-08-08 18:55:53 -0500

    studio2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  memberBeingEdited: object = null;
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -18,4 +18,19 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 }
